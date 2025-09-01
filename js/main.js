10260461d28d282d098475bf08adfd29acd94f54
ogabrieldias <gabrielbielbernardino@gmail.com>
2025-09-01 13:57:41 -0300

    Commit

--- js/main.js
+++ js/main.js
@@ -4,3 +4,29 @@ document.querySelectorAll('.card-seta').forEach(seta => {
         cardInfo.classList.toggle('open');
     });
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+let elemScroll = document.querySelector(".scroll");
+let elemContainer = elemScroll.querySelector(".scroll__container");
+let elemFilhos = Array.from(elemContainer.children);
+
+elemFilhos.forEach((item) => {
+  let elemDuplicado = item.cloneNode(true);
+  elemDuplicado.setAttribute("aria-hidden", true);
+  elemContainer.appendChild(elemDuplicado);
+});
